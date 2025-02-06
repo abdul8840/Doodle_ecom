62efd5e72939bf79ab741c7d29f744184a70ce95
abdul8840 <abdul14941naseer@gmail.com>
2025-02-06 12:38:12 +0530

    create update,get,get all and delete product api route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 import Product from "../models/productModel.js";
 import slugify from 'slugify';
+import validateMongoDbId from "../utils/validateMongoDbId.js";
 
 export const createProduct = async (req, res) => {
   try {
@@ -7,7 +8,92 @@ export const createProduct = async (req, res) => {
       req.body.slug = slugify(req.body.title);
     }
     const newProduct = await Product.create(req.body);
-    res.json(newProduct);
+    res.status(201).json({ message: "Product created successfully", newProduct });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to create product", error });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const updateProduct = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json({ message: "Product updated successfully", updateProduct });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update product", error });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    res.status(200).json({ message: "Product deleted successfully", deletedProduct });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete product", error });
+  }
+};
+
+export const getaProduct = async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const findProduct = await Product.findById(id).populate("color");
+    res.json(findProduct);
+  } catch (error) {
+    res.status(200).status(500).json({ message: "Failed to find product", error });
+  }
+};
+
+export const getAllProduct = async (req, res) => {
+  try {
+    // Filtering
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Product.find(JSON.parse(queryStr));
+
+    // Sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // limiting the fields
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // pagination
+
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) throw new Error("This Page does not exists");
+    }
+    const product = await query;
+    res.json(product);
   } catch (error) {
     throw new Error(error);
   }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express'
-import { createProduct } from '../controllers/productController.js'
+import { createProduct, deleteProduct, getAllProduct, getaProduct, updateProduct } from '../controllers/productController.js'
 
 const router = express.Router()
 
 router.post('/create-product', createProduct);
+router.get("/get-product/:id", getaProduct);
+router.put("/update-product/:id", updateProduct);
+router.delete("/delete-product/:id", deleteProduct);
+
+router.get("/getall-product", getAllProduct);
 
 export default router;
